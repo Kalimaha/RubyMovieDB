93ef0a977b363a1be65602e7dbe4e2ac16908752
Kalimaha <guido.barbaglia@gmail.com>
2016-04-11 09:12:24 +1000

    Fix Coveralls?

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+# Coveralls
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'coveralls'
-
-# Coveralls
-Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
