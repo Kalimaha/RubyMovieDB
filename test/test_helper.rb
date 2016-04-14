7a0f17e0ae58d0aba1236e10d47115843e080406
Kalimaha <guido.barbaglia@gmail.com>
2016-04-14 11:57:36 +1000

    Coveralls added.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+# Coveralls
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
