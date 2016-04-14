00b454c415d6f206ce602b25037ceabf9a725d01
Kalimaha <guido.barbaglia@gmail.com>
2016-04-14 15:33:21 +1000

    Director model added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413045850) do
+ActiveRecord::Schema.define(version: 20160414052838) do
 
   create_table "directors", force: :cascade do |t|
     t.string   "first_name"
@@ -19,10 +19,7 @@ ActiveRecord::Schema.define(version: 20160413045850) do
     t.date     "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "movies_id"
   end
-
-  add_index "directors", ["movies_id"], name: "index_directors_on_movies_id"
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
